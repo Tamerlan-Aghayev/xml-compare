ecd243bcbc98d2a43d4a09168398b8bd530a75cd
Tamerlan <tamerlan.aghayev@gmail.com>
2023-04-29 20:07:31 +0400

    salam

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) throws Exception{
 
 //        findDifference("C:\\Users\\HP\\Desktop\\task\\pom\\text.xml", "C:\\Users\\HP\\Desktop\\task\\pom\\fake.xml","C:\\Users\\HP\\Desktop\\task\\pom\\result.xlsx" );
-        compareXMLWithTxt("C:\\Users\\HP\\Desktop\\task\\notwellformed\\text.xml", "C:\\Users\\HP\\Desktop\\task\\notwellformed\\fake.xml", "C:\\Users\\HP\\Desktop\\task\\notwellformed\\text.txt", "C:\\Users\\HP\\Desktop\\task\\notwellformed\\fake.txt", "C:\\Users\\HP\\Desktop\\task\\notwellformed\\difference.xlsx");
+        compareXMLWithTxt("C:\\Users\\HP\\Desktop\\task\\pom\\text.xml", "C:\\Users\\HP\\Desktop\\task\\pom\\fake.xml", "C:\\Users\\HP\\Desktop\\task\\pom\\text.txt", "C:\\Users\\HP\\Desktop\\task\\pom\\fake.txt", "C:\\Users\\HP\\Desktop\\task\\pom\\difference.xlsx");
+//        compherensive("C:\\Users\\HP\\Desktop\\task\\pom\\text.xml", "C:\\Users\\HP\\Desktop\\task\\pom\\fake.xml");
         System.out.println("hi");
     }
 }
--- src/main/java/org/example/file/FileWork.java
+++ src/main/java/org/example/file/FileWork.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
@@ -17,11 +18,123 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-
+//salam
 
 
 public class FileWork {
-    //this method checks whether the file exist or not. It gets xml file path as input
+// the two first methods are for your use. The others are the methods that is used in these two methods
+    //input: first xml filepath(xml1), second xml filepath(xml2), and the filepath that excel is gonna be(where we want)
+    //biz foldere girende orda yuxarida "C:\\Users\\HP\\Desktop" falan olur, fayl 
+    // olan papkaya gelirik, sonra ardina faylinadi.extension(bizde bele olacaq: example.xml 
+    //meselen "C:\\Users\\HP\\Desktop\\task\\notwellformed\\text.xml" siz iki \ qoyun (\\). 
+    
+    public static void findDifference(String XML1, String XML2, String excelFilePath)throws Exception{
+        if(!(fileExists(XML1)&& fileExists(XML2))) {
+            return;
+        }
+        if(!(fileWellFormCheck(XML1) && fileWellFormCheck(XML2))){
+            return;
+        }
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        workbook=convertXMLToExcel(workbook, XML1, "BeforeCode");
+        workbook=convertXMLToExcel(workbook, XML2, "AfterCode");
+        workbook=getDifferenceShell(workbook);
+        File outputFile = new File(excelFilePath);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+        compareExcels(excelFilePath);
+
+    }
+    
+    
+    //burda ise input kimi istediyimiz iki xml faylinin filepath-ini, onlar text-e cevrilecek deye hemin txt fayllarinin olmagini
+    //istediyimiz yerler ve excelin olmasini istediyimiz addresi qeyd edirik. Mainde qeyd etdiyim yazilara baxa bilersiz.
+    public static void compareXMLWithTxt(String xml1,String xml2, String txt1, String txt2, String outputFile) throws Exception{
+        if(  fileExists(xml1) && fileExists( xml2)) {
+            int index=-1;
+            File file = new File(xml1);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            Document doc = factory.newDocumentBuilder().parse(file);
+            NodeList nodeList=doc.getElementsByTagName("applicationID");
+            int size= nodeList.getLength();
+
+            // Load the contents of the input text files into lists of strings
+            List<String> lines1 = loadFileContents(xml1);
+            List<String> lines2 = loadFileContents(xml2);
+            File output1 = new File(txt1);
+            File output2 = new File(txt2);
+            FileWriter writer1 = new FileWriter(output1);
+            FileWriter writer2 = new FileWriter(output2);
+            // Create a new Excel workbook and sheet to store the differences
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Differences");
+
+            // Add column headers to the sheet
+            Row headerRow = sheet.createRow(0);
+            headerRow.createCell(0).setCellValue("Application ID");
+            headerRow.createCell(1).setCellValue("Tag path");
+            headerRow.createCell(2).setCellValue("BeforeCode");
+            headerRow.createCell(3).setCellValue("AfterCode");
+
+            // Compare the contents of the two input files and write any differences to the Excel sheet
+            int rowNumber = 1; // Start at row 1 (since row 0 is for headers)
+            for (int i = 0; i < lines1.size(); i++) {
+                String line1 = lines1.get(i);
+                String line2 = lines2.get(i);
+                String[] tagAndCode1 = line1.split(">");
+                String[] tagAndCode2 = line2.split(">");
+
+                String tagname = tagAndCode1[0].split("<")[1].trim();
+                if (tagname.equals("applicationID")) {index++;}
+                if (!line1.equals(line2)) {
+                    // The lines are different, so split them into tags and codes
+                    String beforeCode = tagAndCode1[1].split("<")[0].trim();
+                    String afterCode = tagAndCode2[1].split("<")[0].trim();
+
+                    // Create a new row in the Excel sheet for this difference
+                    Row dataRow = sheet.createRow(rowNumber++);
+
+                    dataRow.createCell(0).setCellValue((nodeList.item(index).getTextContent()));
+                    dataRow.createCell(1).setCellValue(getNodePath(doc.getElementsByTagName(tagname).item(index)));
+                    try {
+                        dataRow.createCell(2).setCellValue(Double.parseDouble(beforeCode));
+                        dataRow.createCell(3).setCellValue(Double.parseDouble(afterCode));
+                    }catch(Exception ex){
+                        dataRow.createCell(2).setCellValue((beforeCode));
+                        dataRow.createCell(3).setCellValue((afterCode));
+                    }
+
+
+                    String lineAdd1 = tagAndCode1[0].split("<")[0] + "<" + tagname + ">" + "***" + beforeCode + "***" + "<" + tagAndCode1[1].split("<")[1];
+                    String lineAdd2 = tagAndCode1[0].split("<")[0] + "<" + tagname + ">" + "***" + afterCode + "***" + "<" + tagAndCode1[1].split("<")[1];
+                    writer1.write(lineAdd1 + "\n");
+                    writer2.write(lineAdd2 + "\n");
+                    continue;
+                }
+                writer1.write(line1 + "\n");
+                writer2.write(line2 + "\n");
+            }
+
+            // Write the Excel workbook to a file
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+            writer2.close();
+            writer1.close();
+        }
+    }
+
+    //Bunnan sonrakilara baxmasazda olar, qalanlari bu iki metodun icinde istifade olunur.
+
+
+
+
+    //checks if file exist
     public static boolean fileExists(String path){
         File file = new File(path);
         if(file.exists()) {
@@ -32,7 +145,12 @@ public class FileWork {
             return false;
         }
     }
-    //this method checks whether the file is in well-form. It gets xml file path as an input
+
+
+
+
+    //this method checks whether the file is in well-form. It gets xml file path as an input.
+    //Also, it is used by other method.
     public static boolean fileWellFormCheck(String path)throws Exception{
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -55,8 +173,9 @@ public class FileWork {
     }
 
 
-    //this method checks two excel sheets, and highlights different cells  with yellow background, it gets excel file path as input
 
+    //this method checks two excel sheets, and highlights different cells  with yellow background,
+    // it gets excel file path as input. It is used by other method, no need to use it.
     public static void compareExcels(String filePath) throws Exception{
 
         FileInputStream file = new FileInputStream(new File(filePath));
@@ -107,7 +226,10 @@ public class FileWork {
         beforeOutput.close();
         workbook.close();
     }
-    //this method is used in compareExcel method
+
+
+
+    //this method is used in compareExcel method. no need to use it
     private static void highlightCell(Sheet sheet, Cell cell) {
         CellStyle style = sheet.getWorkbook().createCellStyle();
         style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
@@ -120,7 +242,10 @@ public class FileWork {
         style.setFont(font);
         cell.setCellStyle(style);
     }
-    //this method adds difference sheet to workbook
+
+
+
+    //this method adds difference sheet to workbook. no need to use
     public static XSSFWorkbook getDifferenceShell( XSSFWorkbook differences) throws  Exception{
 
         DataFormatter formatter = new DataFormatter();
@@ -203,71 +328,12 @@ public class FileWork {
     }
     return differences;
 }
-//this method will compare xmls by converting them to txt. It is alternative way to use if comparing by excel doesn't work.
-    //It gets xml files' pathes, pathes that txt files going to be, and path that excel file going to be.
-    public static void compareXMLWithTxt(String xml1,String xml2, String txt1, String txt2, String outputFile) throws Exception{
 
 
-    // Load the contents of the input text files into lists of strings
-    List<String> lines1 = loadFileContents(xml1);
-    List<String> lines2 = loadFileContents(xml2);
-    File output1 = new File(txt1);
-    File output2 = new File(txt2);
-    FileWriter writer1 = new FileWriter(output1);
-    FileWriter writer2 = new FileWriter(output2);
-    // Create a new Excel workbook and sheet to store the differences
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("Differences");
-
-    // Add column headers to the sheet
-    Row headerRow = sheet.createRow(0);
-    headerRow.createCell(0).setCellValue("Row");
-    headerRow.createCell(1).setCellValue("Tagname");
-    headerRow.createCell(2).setCellValue("BeforeCode");
-    headerRow.createCell(3).setCellValue("AfterCode");
-
-    // Compare the contents of the two input files and write any differences to the Excel sheet
-    int rowNumber = 1; // Start at row 1 (since row 0 is for headers)
-    for (int i = 0; i < lines1.size(); i++) {
-        String line1 = lines1.get(i);
-        String line2 = lines2.get(i);
-        if (!line1.equals(line2)) {
-            // The lines are different, so split them into tags and codes
-            String[] tagAndCode1 = line1.split(">");
-            String[] tagAndCode2 = line2.split(">");
-
-            String tagname = tagAndCode1[0].split("<")[1].trim();
-            String beforeCode = tagAndCode1[1].split("<")[0].trim();
-            String afterCode = tagAndCode2[1].split("<")[0].trim();
-
-            // Create a new row in the Excel sheet for this difference
-            Row dataRow = sheet.createRow(rowNumber++);
-            dataRow.createCell(0).setCellValue(i + 1);
-            dataRow.createCell(1).setCellValue(tagname);
-            dataRow.createCell(2).setCellValue(beforeCode);
-            dataRow.createCell(3).setCellValue(afterCode);
 
 
-            String lineAdd1=tagAndCode1[0].split("<")[0]+"<"+tagname+">"+"***"+beforeCode+"***"+"<"+tagAndCode1[1].split("<")[1];
-            String lineAdd2=tagAndCode1[0].split("<")[0]+"<"+tagname+">"+"***"+afterCode+"***"+"<"+tagAndCode1[1].split("<")[1];
-            writer1.write(lineAdd1+"\n");
-            writer2.write(lineAdd2+"\n");
-            continue;
-        }
-        writer1.write(line1+"\n");
-        writer2.write(line2+"\n");
-    }
 
-    // Write the Excel workbook to a file
-    FileOutputStream outputStream = new FileOutputStream(outputFile);
-    workbook.write(outputStream);
-    workbook.close();
-    outputStream.close();
-    writer2.close();
-    writer1.close();
-
-}
-    //used in compareXMLWithTxt method. Input is txt file path
+    //used in compareXMLWithTxt method. Input is txt file path. no need to use
     private static List<String> loadFileContents(String fileName) throws Exception {
         List<String> lines = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -278,8 +344,12 @@ public class FileWork {
         reader.close();
         return lines;
 }
-//it converts xml to txt, if needed.
-    public static void convertXMLToTxt(String xml, String txt) throws  Exception{
+
+
+
+
+//it converts xml to txt, if needed. no need.
+    public static void convertXMLToTxt(String xml, String txt) {
         try {
             // Open the input file
             File inputFile = new File(xml);
@@ -308,7 +378,13 @@ public class FileWork {
         }
     }
 
+
+
+
+
+
     //it converts xml to excel ,then adds them to workbook sheet given as input
+    //no need to use
     public static XSSFWorkbook convertXMLToExcel(XSSFWorkbook workbook,String XMLPath, String sheetName) throws Exception{
         // Replace with the path to your XML file
         // Replace with the path to your XML file
@@ -385,6 +461,8 @@ public class FileWork {
 //        outputStream.close();
         return workbook;
     }
+
+
 //used in convertXMLTOExcel method
     private static List<String> getTagNames(Element element) throws Exception {
         NodeList nodeList = element.getChildNodes();
@@ -403,27 +481,85 @@ public class FileWork {
     }
     //method, that creates excel file with three sheets with beforecode, aftercode, and differences. Also highlights the difference
     //It gets xml files' pathes and file path that excel file going to be.
-    public static void findDifference(String XML1, String XML2, String excelFilePath)throws Exception{
-        if(!(fileExists(XML1)&& fileExists(XML2))) {
-            return;
+    public static String getNodePath(Node node) {
+        String path = "/" + node.getNodeName();
+        Node parent = node.getParentNode();
+        while (parent != null && parent.getNodeType() != Node.DOCUMENT_NODE) {
+            path = "/" + parent.getNodeName() + path;
+            parent = parent.getParentNode();
         }
-        if(!(fileWellFormCheck(XML1) && fileWellFormCheck(XML2))){
-            return;
+        return path;
+    }
+    public static List<String[]> compareNodes(Node node1, Node node2) {
+        List<String[]> differences = new ArrayList<>();
+        if (!node1.getNodeName().equals(node2.getNodeName())) {
+            return differences;
         }
+        if (node1.getNodeType() == Node.TEXT_NODE && node2.getNodeType() == Node.TEXT_NODE) {
+            String data1 = node1.getNodeValue().trim();
+            String data2 = node2.getNodeValue().trim();
+            if (!data1.equals(data2)) {
+                String[] difference = new String[4];
+                difference[0] = node1.getParentNode().getAttributes().getNamedItem("id").getNodeValue();// Assumes that the ID is an attribute of the parent node
+                difference[1] = getNodePath(node1);
+                difference[2] = data1;
+                difference[3] = data2;
+                differences.add(difference);
+            }
+        } else {
+            NodeList children1 = node1.getChildNodes();
+            NodeList children2 = node2.getChildNodes();
+            for (int i = 0; i < children1.getLength() && i < children2.getLength(); i++) {
+                Node child1 = children1.item(i);
+                Node child2 = children2.item(i);
+                differences.addAll(compareNodes(child1, child2));
+            }
+        }
+        return differences;
+    }
+    public static void compherensive(String xml1, String xml2) {
+        try {
+            // Load the XML files
+            File file1 = new File(xml1);
+            File file2 = new File(xml2);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc1 = dBuilder.parse(file1);
+            Document doc2 = dBuilder.parse(file2);
+            doc1.getDocumentElement().normalize();
+            doc2.getDocumentElement().normalize();
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
+            // Compare the XML files
+            List<String[]> differences = compareNodes(doc1.getDocumentElement(), doc2.getDocumentElement());
 
-        workbook=convertXMLToExcel(workbook, XML1, "BeforeCode");
-        workbook=convertXMLToExcel(workbook, XML2, "AfterCode");
-        workbook=getDifferenceShell(workbook);
-        File outputFile = new File(excelFilePath);
-        FileOutputStream outputStream = new FileOutputStream(outputFile);
-        workbook.write(outputStream);
-        workbook.close();
-        outputStream.close();
-        compareExcels(excelFilePath);
+            // Write the differences to an Excel file
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Differences");
+            int rowNumber = 0;
+            Row headerRow = sheet.createRow(rowNumber++);
+            headerRow.createCell(0).setCellValue("Application ID");
+            headerRow.createCell(1).setCellValue("Tag Path");
+            headerRow.createCell(2).setCellValue("Data in First XML");
+            headerRow.createCell(3).setCellValue("Data in Second XML");
+            for (String[] difference : differences) {
+                Row row = sheet.createRow(rowNumber++);
+                row.createCell(0).setCellValue(difference[0]);
+                row.createCell(1).setCellValue(difference[1]);
+                row.createCell(2).setCellValue(difference[2]);
+                row.createCell(3).setCellValue(difference[3]);
+            }
 
-}
+            // Save the Excel file
+            FileOutputStream outputStream = new FileOutputStream("differences.xlsx");
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+
+            System.out.println("Differences saved to differences.xlsx.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
